5529cef32844f26548d0c2943306a256611930c7
Łukasz Tyczka <lukasz@tyczka.net>
2021-03-28 22:09:38 +0200

    Preperation for Router

---
+++ src/AnotherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AnotherPage() {
+    return (
+        <div>
+            <h1>AnotherPage</h1>
+        </div>
+    );
+}
+
+export default AnotherPage;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.scss';
 import FirstComponent from './components/FirstComponent';
+import Search from './components/Search';
+import Header from './Header';
+import Home from './Home';
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <h1>Hello </h1>
             <FirstComponent displayText="Co tam u Ciebie?" />
+            <Search />
+            <Home />
         </div>
     );
 }
